c8b72d65ba6779e8275f2a1bd7aab9e11bd6ed22
georgeowusu <gkojunior@gmail.com>
2024-01-09 10:36:21 -0500

    connected front and backend

--- backend/index.js
+++ backend/index.js
@@ -8,19 +8,17 @@ app.use(cors({ origin: true }))
 
 app.post('/authenticate', async (req, res) => {
 	const { username } = req.body
-
+	// Get or create user on Chat Engine!
 	try {
 		const r = await axios.put(
-			'https://api.chatenhine.io/uses/',
+			'https://api.chatengine.io/users/',
 			{ username: username, secret: username, first_name: username },
-			{headers: {"private-key": "08fe7717-41d8-4242-a035-86279723e939"}}
+			{ headers: { 'Private-Key': '08fe7717-41d8-4242-a035-86279723e939' } }
 		)
 		return res.status(r.status).json(r.data)
 	} catch (e) {
 		return res.status(e.response.status).json(e.response.data)
-
 	}
-	return res.json({ username: username, secret: 'sha256...' })
 })
 
 app.listen(3001)
